ae69b50b3dbeaeb5422c8abb852a32e53a8134fe
Ermia Razavi <151619521+ErmiaRZV@users.noreply.github.com>
2025-10-08 21:28:58 +0330

    Add files via upload

---
+++ temperature/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Temperature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double centigrade, fahrenheit;
+
+            Console.Write("Enter centigrade : ");
+            centigrade = double.Parse(Console.ReadLine());
+
+            fahrenheit = centigrade * 1.8 + 32;
+
+            Console.WriteLine($"{centigrade} centigrade =  {fahrenheit} fahrenheit");
+
+            Console.ReadKey();
+
+        }
+    }
+}
