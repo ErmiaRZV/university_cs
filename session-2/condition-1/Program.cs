ca8b75bedf8deff73e899aff6d788726a4ec0d21
Ermia Razavi <151619521+ErmiaRZV@users.noreply.github.com>
2025-10-09 15:55:17 +0330

    Add files via upload

---
+++ session-2/condition-1/Program.cs
@@ -0,0 +1,19 @@
+﻿﻿using System;
+
+namespace condition1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("Enter a number : ");
+            num = Int32.Parse(Console.ReadLine());
+
+            if (num < 10 && num%2 == 0)
+                Console.WriteLine("TRUE!");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ session-2/condition-2/Program.cs
@@ -0,0 +1,25 @@
+﻿﻿using System;
+
+namespace condition2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num ;
+            Console.Write("Enter a number : ");
+            num = Int32.Parse(Console.ReadLine());
+
+            if (num % 7 == 0)
+                Console.WriteLine("YES!");
+            else
+            {
+                num *= 3;
+                Console.WriteLine(num);
+            }
+
+                Console.ReadKey();
+
+        }
+    }
+}
