c892641de719add7c76d6ade00b4fc71b951ed20
Ermia Razavi <151619521+ErmiaRZV@users.noreply.github.com>
2025-10-30 02:03:30 +0330

    Add files via upload

---
+++ session-4/even-numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿﻿﻿﻿using System;
+
+namespace evenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int evens = 0;
+            int odds = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Enter your number : ");
+                number = Int32.Parse(Console.ReadLine());
+                if (number % 2 == 0)
+                {
+                    evens++;
+                }
+                else
+                {
+                    odds++;
+                }
+
+
+            }
+            Console.WriteLine($"odds : {odds} \t  evens : {evens} ");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ session-4/is-two/Program.cs
@@ -0,0 +1,37 @@
+﻿﻿﻿﻿using System;
+
+namespace isTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number ;
+            bool isTwo = false;
+            int locTwo = -1;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Enter your number : ");
+                number = Int32.Parse(Console.ReadLine());
+                if (number == 2)
+                {
+                    isTwo = true;
+                    locTwo = i + 1;
+                }
+
+
+
+            }
+            
+            if (isTwo)
+            {
+                Console.WriteLine($"I found 2 in your number {Convert.ToString(locTwo)} input");
+            }
+            else
+            {
+                Console.WriteLine("I didnt find 2 . you didnt enter 2");
+            }
+            Console.ReadKey();
+        }
+    }
+}
