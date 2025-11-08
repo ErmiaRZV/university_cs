163e5fd352ce38ecc8f2fdd93a739c0e3485c6f4
Ermia Razavi <151619521+ErmiaRZV@users.noreply.github.com>
2025-11-08 04:20:14 +0330

    Add files via upload

---
+++ session-5/triangle/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Function
+{
+    class Program
+    {
+        public static int mohitFunc(int zel1, int zel2, int zel3)
+        {
+            return zel1 + zel2 + zel3;
+        }
+        public static int masahatFunc(int ghaede, int ertefa)
+        {
+            return (ghaede * ertefa) / 2;
+        }
+       
+       
+
+        static void Main(string[] args)
+        {
+            int zel1, zel2, zel3, mohit, masahat, ghaede, ertefa;
+
+            Console.Write("Enter your fist zel : ");
+            zel1 = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter your second zel : ");
+            zel2 = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter your third zel : ");
+            zel3 = Int32.Parse(Console.ReadLine());
+            mohit = mohitFunc(zel1, zel2, zel3);
+            Console.WriteLine($"mohit = {mohit}");
+
+            Console.Write("Enter your ghaede : ");
+            ghaede = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter your ertefa : ");
+            ertefa = Int32.Parse(Console.ReadLine());
+            masahat = masahatFunc(ghaede, ertefa);
+            Console.WriteLine($"masahat = {masahat} ");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ session-5/zarb/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Zarb
+{
+    class Program
+    {
+        public static int Zarb(int a, int b)
+        {
+            if (b == 0)
+            return 0;
+            return a + Zarb(a, b-1);
+        }
+
+
+       
+
+        static void Main(string[] args)
+        {
+            int num1, num2, result;
+
+            Console.Write("Enter your first number : ");
+            num1 = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter your second number : ");
+            num2 = Int32.Parse(Console.ReadLine()); 
+            result = Zarb(num1, num2);
+            Console.WriteLine($"{num1} * {num2} = {result}");
+            Console.ReadKey();
+        }
+    }
+}
